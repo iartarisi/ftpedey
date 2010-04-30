31f8580521958ee3eff56278e66606af9bdb0552
Ionuț Arțăriși (mapleoin) <mapleoin@lavabit.com>
2010-04-30 17:58:07 +0300

    Infected sources with the GPL virus

--- ftpedey.py
+++ ftpedey.py
@@ -1,11 +1,26 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+# This file is part of ftpedey.
+# Copyright (c) 2010 Ionuț Arțăriși
+
+# ftpedey is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Affero General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+
+# ftpedey is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with ftpedey.  If not, see <http://www.gnu.org/licenses/>.
 
 import gtk
 import gtk.glade
 import gnome.ui
 import pygtk
 import client
-
 
 class FtpedeyGTK:
     def __init__(self):
